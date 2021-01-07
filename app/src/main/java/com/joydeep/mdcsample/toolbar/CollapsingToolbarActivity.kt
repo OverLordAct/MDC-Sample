f0627b37160fbbcf3c669ff6918a02dd0b9aa672
JOYDEEP DAS <joydeep.das43@gmail.com>
2021-01-07 13:15:16 +0530

    Added ScrollingToolbarActivity

---
+++ app/src/main/java/com/joydeep/mdcsample/toolbar/CollapsingToolbarActivity.kt
@@ -0,0 +1,12 @@
+package com.joydeep.mdcsample.toolbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joydeep.mdcsample.R
+
+class CollapsingToolbarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collapsing_toolbar)
+    }
+}
